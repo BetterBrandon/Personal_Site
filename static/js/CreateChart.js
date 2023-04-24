78ad1a37f22f4941feee9783f753f24d782ca124
BRANDON_BISCEGLIA <bbisceglia10@gmail.com>
2023-04-24 12:46:04 -0400

    moved everything via express

---
+++ static/js/CreateChart.js
@@ -0,0 +1,14 @@
+// import Chart from "chart.js/auto";
+
+import { MyChart } from "./MyChart";
+
+// Chart.defaults.font.size = 16;
+
+const language_button = document.getElementById("languages");
+const frame_button = document.getElementById("frame");
+
+language_button.addEventListener("click", function () {
+  console.log("Clicked the language button");
+});
+
+console.log("Script is working");
