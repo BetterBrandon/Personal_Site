1d7b942e3dfcf177e03fb1b4f76180b17a980472
BRANDON_BISCEGLIA <bbisceglia10@gmail.com>
2023-04-25 15:15:46 -0400

    Created animation effect for elements while scroll

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -14757,7 +14757,26 @@ const c = new MyChart();
 // Get the chart buttons from index.ejs
 const language_button = document.getElementById("languages");
 const frame_button = document.getElementById("frame");
+// Get the skills are <p> where on mobile the skills will be a string, not graph
 const skills_area = document.getElementById("skills-area");
+
+// Animation for elements to appear and move up as being scrolled down
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
 
 // Langauge Button
 language_button.addEventListener("click", function () {
@@ -14771,11 +14790,12 @@ frame_button.addEventListener("click", function () {
   skills_area.innerText = "Node.JS, Tailwind, MongoDB, Axios";
 });
 
-console.log("Script is working");
-
 // When window is loaded, by default have the pragramming langauges chart show
+// On mobile, will just display text showing programming languages
 window.onload = () => {
   c.createLanguageChart();
+  skills_area.innerText = "Java, CSS";
+  reveal();
 };
 
 },{"./MyChart.js":6}],6:[function(require,module,exports){
