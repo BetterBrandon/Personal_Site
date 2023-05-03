3ddd54fb8de9d89c9b50548127e30d9a1043b53f
BRANDON_BISCEGLIA <bbisceglia10@gmail.com>
2023-05-03 15:10:36 -0400

    Removed Helmet for now

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const helmet = require("helmet");
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "static")));
-
-app.use(helmet());
 
 app.get("/", (request, response) => {
   response.render("index");
